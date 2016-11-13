6e949d2ba5d0d8ce40618b5dddc0783ac6ecb282
bgeils <bgeils@iastate.edu>
2016-11-13 03:26:18 -0500

    here goes nothing

--- app.py
+++ app.py
@@ -1,40 +1,20 @@
 import os
 
-from flask import Flask, render_template, request, redirect, url_for
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, redirect, url_for
+
+from BSParser import wiki
 
 app = Flask(__name__)
 
-DATABASE_URL = os.environ.get('DATABASE_URL', 'sqlite:////tmp/flask_app.db')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
-db = SQLAlchemy(app)
-
-
-class User(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(100))
-  email = db.Column(db.String(100))
-
-  def __init__(self, name, email):
-    self.name = name
-    self.email = email
-
-db.create_all()
-
-
-@app.route('/')
+@app.route('/', methods=["GET",'POST'])
 def index():
-  return render_template('index.html', users=User.query.all())
-
-
-@app.route('/user', methods=['POST'])
-def user():
   if request.method == 'POST':
-    u = User(request.form['name'], request.form['email'])
-    db.session.add(u)
-    db.session.commit()
-  return redirect(url_for('index'))
+    page = request.args.get('page')
+    return wiki(page)
+  return render_template('index.html')
+
+
 
 if __name__ == '__main__':
   port = int(os.environ.get('PORT', 5000))
